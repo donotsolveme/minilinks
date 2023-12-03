f348b0693b092c6aef7e9e9b4c3f895eaea12692
帆海 <151527242+donotsolveme@users.noreply.github.com>
2023-12-03 17:03:33 +0900

    improve

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import re
 import sqlite3
 from time import time
 from typing import Optional
+
 import uvicorn
-from fastapi import FastAPI, HTTPException, Request, Response, status, Security
+from fastapi import FastAPI, HTTPException, Request, Response, Security, status
 from fastapi.responses import RedirectResponse
 from fastapi.security import APIKeyHeader
-import config 
+
+import config
+
 app = FastAPI()
 
 # db init
@@ -20,22 +23,28 @@ db.close()
 # api key
 api_key_header = APIKeyHeader(name="X-Api-key")
 
+
 def get_api_key(api_key_header: str = Security(api_key_header)) -> str:
     if api_key_header == config.SECRET_KEY:
         return api_key_header
     raise HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid or missing API key"
+        status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid or missing API key."
     )
 
+def add_http(url: str) -> str: 
+    if re.match(url, r"^\w+:\/\/") is None:
+        return "http://" + url
+    return url
 
 @app.get("/{id}")
 def redirect(id: str, response: Response):
     with sqlite3.connect("minilinks.db") as db:
         cur = db.execute("SELECT url FROM links WHERE id=?", (id,))
-        url = cur.fetchone()[0]
-        print(url)
-        if url is None:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Link not found!")
+        try:
+            url = cur.fetchone()[0]
+        except TypeError:
+            response.status_code = status.HTTP_404_NOT_FOUND
+            return "Link not found."
         return RedirectResponse(url, status_code=status.HTTP_301_MOVED_PERMANENTLY)
 
 
@@ -47,20 +56,24 @@ def add(
     note: str | None = None,
     api_key: str = Security(get_api_key),
 ):
-    if re.match(url, r"^\w+:\/\/") is None:
-        url = "http://" + url
+    url = add_http(url)
     with sqlite3.connect("minilinks.db") as db:
         now = int(time())
-        db.execute(
-            "INSERT INTO links VALUES(?, ?, ?, ?, ?)",
-            (
-                id,
-                url,
-                note,
-                now,  # created_at
-                now,  # updated_at
-            ),
-        )
+        try:
+            db.execute(
+                "INSERT INTO links VALUES(?, ?, ?, ?, ?)",
+                (
+                    id,
+                    url,
+                    note,
+                    now,  # created_at
+                    now,  # updated_at
+                ),
+            )
+        except sqlite3.IntegrityError:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT, detail="That ID is in use."
+            )
         db.commit()
     resp = {
         "id": id,
@@ -74,7 +87,7 @@ def add(
 
 
 @app.patch("/api")
-def edit(
+def update(
     request: Request,
     id: str,
     url: Optional[str] = None,
@@ -83,6 +96,7 @@ def edit(
 ):
     with sqlite3.connect("minilinks.db") as db:
         if url is not None:
+            url = add_http(url)
             db.execute("UPDATE links SET url = ? WHERE id = ?", (url, id))
         if note is not None:
             db.execute("UPDATE links SET note = ? WHERE id = ?", (note, id))
@@ -112,13 +126,21 @@ def edit(
 def delete(id: str, api_key: str = Security(get_api_key)):
     with sqlite3.connect("minilinks.db") as db:
         cur = db.execute("SELECT * FROM links WHERE id = ?", (id,))
-        print(cur.fetchone())
-        if cur.fetchone():
-            db.execute("DELETE FROM links WHERE EXISTS(SELECT * FROM links WHERE id = ?)", (id,))
+        data = cur.fetchone()
+        if data is not None:
+            db.execute(
+                "DELETE FROM links WHERE id = ?",
+                (id,),
+            )
         else:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="URL not found")
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, detail="URL not found."
+            )
     return {"deleted_id": id}
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, port=8000,)
+    uvicorn.run(
+        app,
+        port=8000,
+    )
