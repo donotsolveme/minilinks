1a44a3617b750fcd4d79f4f049967f7673b6178c
帆海 <151527242+donotsolveme@users.noreply.github.com>
2023-12-03 15:17:36 +0900

    add iroiro

--- main.py
+++ main.py
@@ -2,18 +2,30 @@ import re
 import sqlite3
 from time import time
 from typing import Optional
-
-from fastapi import FastAPI, Request, Response, status
+import uvicorn
+from fastapi import FastAPI, HTTPException, Request, Response, status, Security
 from fastapi.responses import RedirectResponse
-from pydantic import BaseModel
-
+from fastapi.security import APIKeyHeader
+import config 
 app = FastAPI()
 
+# db init
 db = sqlite3.connect("minilinks.db")
 db.execute(
     "CREATE TABLE IF NOT EXISTS links(id TEXT UNIQUE, url TEXT, note TEXT, created_at INTEGER, updated_at INTEGER)"
 )
 db.close()
+
+
+# api key
+api_key_header = APIKeyHeader(name="X-Api-key")
+
+def get_api_key(api_key_header: str = Security(api_key_header)) -> str:
+    if api_key_header == config.SECRET_KEY:
+        return api_key_header
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid or missing API key"
+    )
 
 
 @app.get("/{id}")
@@ -23,13 +35,18 @@ def redirect(id: str, response: Response):
         url = cur.fetchone()[0]
         print(url)
         if url is None:
-            response.status_code = status.HTTP_404_NOT_FOUND
-            return "Link not found!"
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Link not found!")
         return RedirectResponse(url, status_code=status.HTTP_301_MOVED_PERMANENTLY)
 
 
 @app.post("/api")
-def add(request: Request, id: str, url: str, note: str | None = None):
+def add(
+    request: Request,
+    id: str,
+    url: str,
+    note: str | None = None,
+    api_key: str = Security(get_api_key),
+):
     if re.match(url, r"^\w+:\/\/") is None:
         url = "http://" + url
     with sqlite3.connect("minilinks.db") as db:
@@ -57,7 +74,13 @@ def add(request: Request, id: str, url: str, note: str | None = None):
 
 
 @app.patch("/api")
-def edit(request: Request, id: str, url: Optional[str] = None, note: str | None = None):
+def edit(
+    request: Request,
+    id: str,
+    url: Optional[str] = None,
+    note: str | None = None,
+    api_key: str = Security(get_api_key),
+):
     with sqlite3.connect("minilinks.db") as db:
         if url is not None:
             db.execute("UPDATE links SET url = ? WHERE id = ?", (url, id))
@@ -86,7 +109,16 @@ def edit(request: Request, id: str, url: Optional[str] = None, note: str | None 
 
 
 @app.delete("/api")
-def delete(id: str):
+def delete(id: str, api_key: str = Security(get_api_key)):
     with sqlite3.connect("minilinks.db") as db:
-        db.execute("DELETE FROM links WHERE id = ?", (id))
+        cur = db.execute("SELECT * FROM links WHERE id = ?", (id,))
+        print(cur.fetchone())
+        if cur.fetchone():
+            db.execute("DELETE FROM links WHERE EXISTS(SELECT * FROM links WHERE id = ?)", (id,))
+        else:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="URL not found")
     return {"deleted_id": id}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000,)
